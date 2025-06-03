252a1b2aad36446ff2f91b7fd9f1c3eaf266fc1e
Afan30 <peacyaffan8877@gmail.com>
2025-06-03 12:23:57 +0530

    second commit

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const Thought = require('./routes/Thoughts');
 const cookieParser = require('cookie-parser');
 const cors = require("cors");
 dotenv.config();
-
+const bodyParser = require("body-parser");
 
 
 const app = express();
@@ -20,17 +20,39 @@ app.use(express.json());
 
 
 
+// app.use(cors({
+//   origin: "http://localhost:5173",
+//   credentials: true,
+// }));
+
+const allowedOrigins = process.env.FRONTEND_URL
 app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true); 
+    } else {
+      callback(new Error('Not allowed by CORS')); 
+    }
+  },
+  credentials: true, 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+  allowedHeaders: ['Content-Type', 'Authorization'], 
 }));
 
-mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err));
+app.use((err, req, res, next) => {
+  if (err) {
+    console.error(err.message);
+    res.status(500).json({ error: err.message });
+  } else {
+    next();
+  }
+});
+
+app.options('*', (req, res) => {
+  res.sendStatus(204); 
+});
+
+app.use(bodyParser.json());
 
 app.use('/', UserRoute);
 app.use('/', AdminRoute);
@@ -39,7 +61,18 @@ app.use('/', CompanyRoute);
 app.use('/', ApplicationRoute);
 app.use('/', Thought);
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the Backend Server!");
+});
 
+module.exports = app;
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
